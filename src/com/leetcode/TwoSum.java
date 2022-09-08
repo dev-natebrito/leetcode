99421d243242c6a80391de5a9299b3a8014c90fb
nate <it.nathanbrito@gmail.com>
2022-09-08 09:45:58 -0300

    TwoSum solution using hashmap

---
+++ src/com/leetcode/TwoSum.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(target - nums[i])) {
+                return new int[]{hash.get(target - nums[i]), i};
+            }
+            hash.put(nums[i], i);
+
+        }
+        return new int[]{};
+    }
+}
+
